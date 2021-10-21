7320605be52a5386041c332d91c29dcb149ae21d
JonasDeOliveira <jonas.doliver@gmail.com>
2021-10-21 10:08:55 -0300

    base para calculadora

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
 import './App.css';
+import Calculator from './main/Calculator';
 
 function App() {
   return (
-    <>
-    </>
+    <div>
+      <h1>Calculator</h1>
+      <Calculator/>
+    </div>
   );
 }
 
